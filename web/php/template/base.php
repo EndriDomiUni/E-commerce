6b2f200db6cbef92f397aa24e24bc1e870a5afea
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-11-14 16:45:10 +0100

    refactoring

--- web/php/template/base.php
+++ web/php/template/base.php
@@ -7,7 +7,7 @@
   <title>E-commerce</title>
   <link rel="stylesheet" href="../css/style.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <script src="../js/base.js" defer></script>
+  <script src="../assets/js/base.js" defer></script>
 </head>
 <body>
   <header class="header">
