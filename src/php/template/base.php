1229b4015a190811eb68d49521302ca15bb04c5b
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2022-11-17 16:46:18 +0100

    Update footer

--- src/php/template/base.php
+++ src/php/template/base.php
@@ -117,17 +117,17 @@ Footer START -->
 			<!-- Row START -->
 			<div class="row g-4">
 
-				<!-- Widget 1 START -->
+				<!-- Widget 1 START 
 				<div class="col-lg-3">
-					<!-- logo -->
+					 #logo -> this is a comment
 					<a href="index.html">
-						<img class="h-40px" src="../assets/images/logo-light.svg" alt="logo">
-					</a>
+
+          </a>
 					<p class="my-3 text-muted">Departure defective arranging rapturous did believe him all had supported.</p>
 					<p class="mb-2"><a href="#" class="text-muted text-primary-hover"><i class="bi bi-telephone me-2"></i>+1234 568 963</a> </p>
 					<p class="mb-0"><a href="#" class="text-muted text-primary-hover"><i class="bi bi-envelope me-2"></i>example@gmail.com</a></p>
 				</div>
-				<!-- Widget 1 END -->
+				 Widget 1 END -->
 
 				<!-- Widget 2 START -->
 				<div class="col-lg-8 ms-auto">
@@ -155,36 +155,13 @@ Footer START -->
 								<li class="nav-item"><a class="nav-link text-muted" href="#">Support</a></li>
 							</ul>
 						</div>
-
-						<!-- Link block -->
-						<div class="col-6 col-md-3">
-							<h5 class="text-white mb-2 mb-md-4">Global Site</h5>
-							<ul class="nav flex-column text-primary-hover">
-								<li class="nav-item"><a class="nav-link text-muted" href="#">India</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#">California</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#">Indonesia</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#">Canada</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#">Malaysia</a></li>
-							</ul>
-						</div>
-
-						<!-- Link block -->
-						<div class="col-6 col-md-3">
-							<h5 class="text-white mb-2 mb-md-4">Booking</h5>
-							<ul class="nav flex-column text-primary-hover">
-								<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-hotel me-2"></i>Hotel</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-plane me-2"></i>Flight</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-globe-americas me-2"></i>Tour</a></li>
-								<li class="nav-item"><a class="nav-link text-muted" href="#"><i class="fa-solid fa-car me-2"></i>Cabs</a></li>
-							</ul>
-						</div>
 					</div>
 				</div>
 				<!-- Widget 2 END -->
 
 			</div><!-- Row END -->
 
-			<!-- Tops Links -->
+			<!-- Tops Links 
 			<div class="row mt-5">
 				<h5 class="mb-2 text-white">Top Links</h5>
 				<ul class="list-inline text-primary-hover lh-lg">
@@ -211,11 +188,11 @@ Footer START -->
 					<li class="list-inline-item"><a href="#" class="text-muted">Share Market</a></li>
 				</ul>
 			</div>
-
+      -->
 			<!-- Payment and card -->
 			<div class="row g-4 justify-content-between mt-0 mt-md-2">
 
-				<!-- Payment card -->
+				<!-- Payment card 
 				<div class="col-sm-7 col-md-6 col-lg-4">
 					<h5 class="text-white mb-2">Payment & Security</h5>
 					<ul class="list-inline mb-0 mt-3">
@@ -225,8 +202,8 @@ Footer START -->
 						<li class="list-inline-item"> <a href="#"><img src="../assets/images/element/expresscard.svg" class="h-30px" alt=""></a></li>
 					</ul>
 				</div>
-
-				<!-- Social media icon -->
+        -->
+				<!-- Social media icon 
 				<div class="col-sm-5 col-md-6 col-lg-3 text-sm-end">
 					<h5 class="text-white mb-2">Follow us on</h5>
 					<ul class="list-inline mb-0 mt-3">
@@ -237,6 +214,7 @@ Footer START -->
 					</ul>
 				</div>
 			</div>
+      -->
 
 			<!-- Divider -->
 			<hr class="mt-4 mb-0">
@@ -246,7 +224,7 @@ Footer START -->
 				<div class="container">
 					<div class="d-lg-flex justify-content-between align-items-center py-3 text-center text-lg-start">
 						<!-- copyright text -->
-						<div class="text-muted text-primary-hover"> Copyrights <a href="#" class="text-muted">©2022 Booking</a>. All rights reserved. </div>
+						<div class="text-muted text-primary-hover"> Copyrights <a href="#" class="text-muted">©2022 Take-it</a>. All rights reserved. </div>
 						<!-- copyright links-->
 						<div class="nav mt-2 mt-lg-0">
 							<ul class="list-inline text-primary-hover mx-auto mb-0">
