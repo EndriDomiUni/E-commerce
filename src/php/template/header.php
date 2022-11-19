0fcc0a374c0e99785cbbb90b33e6564c8628cea7
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-11-19 11:59:32 +0100

    refactor code

--- src/php/template/header.php
+++ src/php/template/header.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg bg-light" id="navbar-main">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.php">
-                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="200.000000pt" height="100.000000pt" viewBox="0 0 1000.000000 623.000000" preserveAspectRatio="xMidYMid meet">
+                <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="100.000000pt" height="100.000000pt" viewBox="0 0 1000.000000 623.000000" preserveAspectRatio="xMidYMid meet">
                     <g transform="translate(0.000000,623.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none" class="nav-icon-item">
                         <path d="M20 3115 l0 -3015 3010 0 3010 0 0 735 0 735 -70 0 -70 0 0 -660 0
                         -660 -2870 0 -2870 0 0 2865 0 2865 2870 0 2870 0 0 -660 0 -660 70 0 70 0 0
@@ -115,7 +115,7 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <!-- start home -->
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">
+                        <a class="nav-link" aria-current="page" href="index.php">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-door-fill nav-icon-item" viewBox="0 0 16 16">
                                 <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
                             </svg>
@@ -136,18 +136,9 @@
                             </div>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end ">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">
-                                    <div class="form-check form-switch">
-                                        <input class="form-check-input" type="checkbox" role="switch" id="btn-dark-mode" onclick="checkForDarkMode(event)">
-                                        <label class="form-check-label" for="btn-dark-mode">Dark Mode</label>
-                                    </div>
-                                </a>
-                            </li>
+                            <li><a class="dropdown-item" href="./view/DashboardView.php">Dashboard</a></li>
+                            <li><a class="dropdown-item" href="#">Notifications</a></li>
+                            <li><a class="dropdown-item" href="#">Ordini</a></li>
                         </ul>
                     </li>
 
@@ -167,7 +158,7 @@
 
                     <!-- start carrello -->
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="shop-cart-view.php">
+                        <a class="nav-link" aria-current="page" href="./view/ShopCartView/">
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart-fill nav-icon-item" viewBox="0 0 16 16">
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                             </svg>
@@ -190,9 +181,9 @@
                             </div>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end ">
-                            <li><a class="dropdown-item" href="login-view.php">Login in</a></li>
-                            <li><a class="dropdown-item" href="signup-view.php">Sign up</a></li>
-                            <li><a class="dropdown-item" href="#">Inbox</a></li>
+                            <li><a class="dropdown-item" href="./view/LoginView.php">Login in</a></li>
+                            <li><a class="dropdown-item" href="./view/SignUpView.php">Sign up</a></li>
+                            <li><a class="dropdown-item" href="./view/Inbox.php">Inbox</a></li>
                             <li>
                                 <hr class="dropdown-divider">
                             </li>
