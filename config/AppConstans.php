e93a4a4ca2cb123c485519b1da31419194e34c77
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2022-12-02 17:22:01 +0100

    Fixed up path and links

--- config/AppConstans.php
+++ config/AppConstans.php
@@ -1,26 +1,18 @@
 <?php
 
 // ROOT 
-define("ROOT", basename(dirname("/")));
+define("ROOT", "/app");
 define("URL_ROOT", isset($_SERVER['HTTPS']) ? "https" : "http" . "://" . $_SERVER['SERVER_NAME'] . ROOT);
 
-// INDEX 
-define("INDEX", ROOT . "\index.php");
-
-
 // ASSETS
-define("CSS", ROOT . "\assets\css");
-define("JS", ROOT . "\assets\js");
-define("IMG", ROOT . "\assets\img");
+define("CSS", ROOT . "/assets/css");
+define("JS", ROOT . "/assets/js");
+define("IMG", ROOT . "/assets/img");
 
 // VIEWS
 define('VIEW_ROOT', URL_ROOT . '/templates/views');
 define("VIEW_USER", VIEW_ROOT . "/user");
 define("VIEW_SELLER", VIEW_ROOT . "/seller");
-define("SIGN-IN", VIEW_USER . "/sign-in.php");
-define("SIGN-UP", VIEW_USER . "/sign-up.php");
-define("INBOX", VIEW_USER . "/inbox.php");
-define("SETTINGS", VIEW_USER . "/settings.php");
 
 // DATABASE
 define("SERVER", "localhost");
@@ -29,6 +21,9 @@ define("PASSWORD", "");
 define("DBNAME", "e-commerce");
 define("PORT", "3306");
 define("CHARSET", "utf8");
+
+// FOLDERS
+define("CONFIG", ROOT . "/config");
 
 // SPECIAL CHARACTERS
 define("EURO", "€");
--- templates/views/utils/navbar.php
+++ templates/views/utils/navbar.php
@@ -1,11 +1,7 @@
-<?php 
-require("./config/AppConstans.php");
-?>
-
 <nav class="navbar navbar-expand-lg bg-light" id="navbar-main">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?php echo ROOT; ?>">
-            <?php echo IMG . "/logo.svg"; ?>
+            <img src="<?php echo IMG;?>/logo.svg" alt="Logo" />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="toogle-hamburger">
             <span class="navbar-toggler-icon"></span>
@@ -15,7 +11,7 @@ require("./config/AppConstans.php");
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <!-- start home -->
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="<?php echo INDEX; ?>">
+                    <a class="nav-link" aria-current="page" href="<?php echo ROOT; ?>">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-house-door-fill nav-icon-item" viewBox="0 0 16 16">
                             <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5Z" />
                         </svg>
@@ -81,9 +77,9 @@ require("./config/AppConstans.php");
                         </div>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end ">
-                        <li><a class="dropdown-item" href="<?php echo SIGN-IN ?>">Login in</a></li>
-                        <li><a class="dropdown-item" href="<?php echo SIGN-UP ?>">Sign up</a></li>
-                        <li><a class="dropdown-item" href="<?php echo INBOX ?>">Inbox</a></li>
+                        <li><a class="dropdown-item" href="./view/LoginView.php">Login in</a></li>
+                        <li><a class="dropdown-item" href="./view/SignUpView.php">Sign up</a></li>
+                        <li><a class="dropdown-item" href="./view/Inbox.php">Inbox</a></li>
                         <li>
                             <hr class="dropdown-divider">
                         </li>
@@ -93,12 +89,12 @@ require("./config/AppConstans.php");
                                     <label class="form-check-label" for="btn-dark-mode">Dark Mode</label>
                                 </div>
                             </a>
-                            <a class="dropdown-item" href="<?php SETTINGS?>">
+                            <a class="dropdown-item" href="settings.php">
                                 <div class="settings-container">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-gear-wide-connected nav-icon-item" viewBox="0 0 16 16">
                                         <path d="M7.068.727c.243-.97 1.62-.97 1.864 0l.071.286a.96.96 0 0 0 1.622.434l.205-.211c.695-.719 1.888-.03 1.613.931l-.08.284a.96.96 0 0 0 1.187 1.187l.283-.081c.96-.275 1.65.918.931 1.613l-.211.205a.96.96 0 0 0 .434 1.622l.286.071c.97.243.97 1.62 0 1.864l-.286.071a.96.96 0 0 0-.434 1.622l.211.205c.719.695.03 1.888-.931 1.613l-.284-.08a.96.96 0 0 0-1.187 1.187l.081.283c.275.96-.918 1.65-1.613.931l-.205-.211a.96.96 0 0 0-1.622.434l-.071.286c-.243.97-1.62.97-1.864 0l-.071-.286a.96.96 0 0 0-1.622-.434l-.205.211c-.695.719-1.888.03-1.613-.931l.08-.284a.96.96 0 0 0-1.186-1.187l-.284.081c-.96.275-1.65-.918-.931-1.613l.211-.205a.96.96 0 0 0-.434-1.622l-.286-.071c-.97-.243-.97-1.62 0-1.864l.286-.071a.96.96 0 0 0 .434-1.622l-.211-.205c-.719-.695-.03-1.888.931-1.613l.284.08a.96.96 0 0 0 1.187-1.186l-.081-.284c-.275-.96.918-1.65 1.613-.931l.205.211a.96.96 0 0 0 1.622-.434l.071-.286zM12.973 8.5H8.25l-2.834 3.779A4.998 4.998 0 0 0 12.973 8.5zm0-1a4.998 4.998 0 0 0-7.557-3.779l2.834 3.78h4.723zM5.048 3.967c-.03.021-.058.043-.087.065l.087-.065zm-.431.355A4.984 4.984 0 0 0 3.002 8c0 1.455.622 2.765 1.615 3.678L7.375 8 4.617 4.322zm.344 7.646.087.065-.087-.065z" />
                                     </svg>
-                                    <label class="form-check-label">Settings</label>
+                                    <label class="form-check-label">Settins</label>
                                 </div>
                             </a>
                         </li>
