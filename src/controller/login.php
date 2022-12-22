7b6f4cc520ab4c0751ba1315f58c22ce25410cd9
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2022-12-22 16:58:34 +0100

    Update login.php

--- src/controller/login.php
+++ src/controller/login.php
@@ -3,18 +3,21 @@ include(dirname(__FILE__) . "");
 require(dirname(__FILE__) . "");
 
 if ((isset($_POST['submit']))) {
-    // Queste sono da mettere come constante globale
-    $sellerNameTable = "venditore";
-    $clientNameTable = "cliente";
     $parameters = array($_POST['email'], $_POST['password'])
     $customerHelper = new CustomerHelper();
     $sellertHelper = new SellerHelper();
     if($customerHelper->checkemail(CUSTOMER), $_POST['email'])
     {
-        $user = $customerHelper->signin($parameters)
+        $response = $customerHelper->signin($parameters)
+        if (is_array($response) && $response["Status"] == ERROR){
+            $error_register = $response["msg"];
+        } else {
+            // da settare l'ultima pagina visualizzatas
+            header("Location: index.php");
+        }
     }
     elseif ($sellertHelper->checkemail(SELLER), $_POST['email'])
     {
-        $parameters = $sellertHelper->signin($parameters);
+        $response = $sellertHelper->signin($parameters);
     }
 }
