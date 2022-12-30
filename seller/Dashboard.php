029fda8e7485f4700d6931ce3333fd05944fea35
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2022-12-30 20:30:29 +0100

    delete seller

--- seller/Dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../src/utility/guestUser.php");
-require_once("../config/AppCostants.php");
-require '../templates/views/seller/dashboard.php';
-$main_Content = ob_get_clean();
-$title = "Dashboard";
-require_once("../templates/baseSeller.php");
--- seller/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once("../config/AppConstants.php");
-ob_start();
-require '../templates/views/seller/index.php';
-$main_Content = ob_get_clean();
-require_once("../templates/baseSeller.php");
