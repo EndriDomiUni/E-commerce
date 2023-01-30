564816eeeb9597893d941a3a21001f31b89fb1fd
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2023-01-30 15:58:27 +0100

    Update dashboard

---
+++ dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+require_once("./config/AppConstants.php");
+require_once("./script/productInsertion.script.php");
+
+ob_start();
+require './templates/views/form/login.php';
+$mainContent = ob_get_clean();
+$title = "Login";
+require_once("./templates/base.php");
