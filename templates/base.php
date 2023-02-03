de05e87a511908e19ab58380fccc409f07eaf1f3
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2023-02-03 17:56:32 +0100

    improve dashboard

--- templates/base.php
+++ templates/base.php
@@ -18,8 +18,8 @@ require_once("./assets/js/script.php");
 </header>
 <!-- ======================= Header END -->
 
-<?php
 
+<?php
 if (isset($_SESSION["Id"])) {
     try {
         $session = new Session($_SESSION["Id"]);
@@ -27,69 +27,31 @@ if (isset($_SESSION["Id"])) {
             if ($title === "Dashboard") {
                 $claimType = $session->getClaimTypeFromId($session->getCurrentUser()[CLAIM_ID]);
                 if ($claimType == CLAIM_SELLER_DESC || $claimType === CLAIM_SELLER_PR0_DESC) {
-                    $sidebar = '<div class="sidebar hide">
-            <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-                    <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-                    <span class="fs-4">Take it</span>
-                </a>
-                <hr>
-                <ul class="nav nav-pills flex-column mb-auto">
-                    <li>
-                        <a href="#" class="nav-link link-white active showContent" >
-                            <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
-                            Inserisci prodotto
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#b1" class="nav-link link-dark showContent">
-                            <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
-                            Modifica prodotto
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link link-dark showContent">
-                            <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-                            Elimina prodotto
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link link-dark showContent">
-                            <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-                            Magazzino
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link link-dark showContent">
-                            <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-                            Articoli in Magazzino
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link link-dark showContent">
-                            <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-                            Finanze
-                        </a>
-                    </li>
-                </ul>
-                <hr>
-                 </div>
-            </div>';
-                    echo $sidebar;
+
+                    echo '<div class="row">
+                                <div id="sidebar" class="col">
+                                </div>
+                                
+                                <div class="col-10">';
+                                    echo $mainContent;
+                    echo '      </div>
+                        </div>';
+
+                    echo '<script src="/app/assets/js/generateSidebar.js" type="text/javascript">',
+                    '</script>';
                 }
+            } else {
+                echo $mainContent;
             }
+        } else {
+            echo $mainContent;
         }
     } catch (Exception $e) {
         echo $e->getMessage();
-
     }
 }
 ?>
 
-
-<!-- ======================= Main content START -->
-<?php echo $mainContent; ?>
-<!-- ======================= Main content END -->
 
 <!-- ======================= Footer START -->
 <?php require("./templates/views/utils/footer.php"); ?>
