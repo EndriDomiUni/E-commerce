1dad34d88ea446007386b8ec3a701f13b683bb83
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2023-02-12 14:35:58 +0100

    update dashboard ui

--- templates/views/user/dashboard.php
+++ templates/views/user/dashboard.php
@@ -1,11 +1,36 @@
+<script
+        src="https://www.gstatic.com/charts/loader.js">
+</script>
+
+
 <!-- Sales Report Section -->
 <div class="container my-5">
     <h2 class="text-center">Sales Report</h2>
     <div class="row">
         <!-- Sales Chart -->
-        <div class="col-md-8">
-            <canvas id="salesChart"></canvas>
+        <div id="myChart" style="max-width:700px; height:400px">
+            <script type="text/javascript">
+                google.charts.load('current',{packages:['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+                    let data = google.visualization.arrayToDataTable([
+                        ['Price', 'Size'],
+                        [50,7],[60,8],[70,8],[80,9],[90,9],[100,9],
+                        [110,10],[120,11],[130,14],[140,14],[150,15]
+                    ]);
+                    let options = {
+                        title: 'House Prices vs Size',
+                        hAxis: {title: 'Square Meters'},
+                        vAxis: {title: 'Price in Millions'},
+                        legend: 'none'
+                    };
+                    let chart = new google.visualization.LineChart(document.getElementById('myChart'));
+                    chart.draw(data, options);
+                }
+            </script>
         </div>
+
         <!-- Order Statistics -->
         <div class="col-md-4">
             <!-- Today's Orders -->
