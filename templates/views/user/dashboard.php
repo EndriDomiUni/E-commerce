481de3c3bfe7fca421bf48fa777e547c0cad3347
Endri Domi <74309245+EndriDomiUni@users.noreply.github.com>
2023-02-12 12:00:08 +0100

    update ui

--- templates/views/user/dashboard.php
+++ templates/views/user/dashboard.php
@@ -1,29 +1,41 @@
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
-<h1>PRova</h1>
-<h2>Magari facciamo una schermata di defualt</h2>
-<h3>Magari facciamo una schermata di defualt 3</h3>
-
+<!-- Sales Report Section -->
+<div class="container my-5">
+    <h2 class="text-center">Sales Report</h2>
+    <div class="row">
+        <!-- Sales Chart -->
+        <div class="col-md-8">
+            <canvas id="salesChart"></canvas>
+        </div>
+        <!-- Order Statistics -->
+        <div class="col-md-4">
+            <!-- Today's Orders -->
+            <div class="card my-3">
+                <div class="card-header">Today's Orders</div>
+                <div class="card-body">
+                    <h5 class="card-title">10</h5>
+                </div>
+            </div>
+            <!-- Current Month Orders -->
+            <div class="card my-3">
+                <div class="card-header">Current Month Orders</div>
+                <div class="card-body">
+                    <h5 class="card-title">100</h5>
+                </div>
+            </div>
+            <!-- Total Orders -->
+            <div class="card my-3">
+                <div class="card-header">Total Orders</div>
+                <div class="card-body">
+                    <h5 class="card-title">1000</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Total Money -->
+    <div class="card my-3">
+        <div class="card-header">Total Money</div>
+        <div class="card-body">
+            <h5 class="card-title">$10000</h5>
+        </div>
+    </div>
+</div>
