43cb7a82950e5af5ea9584c3f19c961cff5ad090
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2023-02-13 18:05:32 +0100

    Update product_card

--- templates/views/components/productCard.php
+++ templates/views/components/productCard.php
@@ -54,8 +54,8 @@
                                                     <p class="card-text">A partire da: ' . EURO . ' ' . $currentProductPrice . '</p>
                                                     <label for="article-quantity" class="form-label">Quantità</label>
                                                     <input type="number" min="1" class="form-control" id="product-name" name="article-quantity"  />
-                                        </div>
-                                        <div class="card-size">';
+                                                </div>
+                                            <div class="card-size">';
                     //debug
                     //echo "product id: ".$product[ID]."</br>";
                     $currentArticles = $session->getArticlesByProductId($product[ID]) != null
@@ -91,19 +91,19 @@
                                 $options[$currentOption[0][ID]] = $currentOption[0];
                             }
                         }
-                        foreach ($variations as $variation) {
-                            echo '<label for="product-variation-' . $variation[ID] . '">' . $variation[NOME] . '</label>';
-                            echo '<select class="form-select" id="product-variation-' . $variation[ID] . '"  name="product-variation-' . $variation[ID] . '" aria-label="Default select example">';
-                            echo '<option value="">--Seleziona opzione--</option>';
-                            foreach ($options as $option) {
-                                if ($option[VARIAZIONE_ID] === $variation[ID]) {
-                                    echo '<option value="option-' . $option[ID] . '">' . $option[VALORE] . '</option>';
-                                }
-                            }
-                            echo '</select>';
-                        }
-                        drawCardFooter();
                     }
+                    foreach ($variations as $variation) {
+                        echo '<label for="product-variation-' . $variation[ID] . '">' . $variation[NOME] . '</label>';
+                        echo '<select class="form-select" id="product-variation-' . $variation[ID] . '"  name="product-variation-' . $variation[ID] . '" aria-label="Default select example">';
+                        echo '<option value="">--Seleziona opzione--</option>';
+                        foreach ($options as $option) {
+                            if ($option[VARIAZIONE_ID] === $variation[ID]) {
+                                echo '<option value="option-' . $option[ID] . '">' . $option[VALORE] . '</option>';
+                            }
+                        }
+                        echo '</select>';
+                    }
+                    drawCardFooter($product[ID]);
                 }
             }
         }
@@ -112,7 +112,7 @@
 </section>
 
 <?php
-function drawCardFooter(): void
+function drawCardFooter($productId): void
 {
     echo '
                     </select>
@@ -120,22 +120,21 @@ function drawCardFooter(): void
                     </div>
                     </div>
                     </div>
-                    </div>
                     <div class="row no-gutters">
                         <div class="col-md-5"></div>
                         <div class="col-md-9">
-                            <button class="btn btn-primary" type="submit" name="add-product-in-wishlist">
+                            <button class="btn btn-primary" type="submit" name="add-product-' . $productId . '-in-wishlist">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                                 </svg>
                             </button>
-                            <button class="btn btn-primary" type="submit" name="add-article-in-cart">
+                            <button class="btn btn-primary" type="submit" name="add-article-' . $productId . '-in-cart">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-fill" viewBox="0 0 16 16">
                                     <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
                                 </svg>
                                 Add to cart
                             </button>
-                            <button class="btn btn-primary" type="submit" name="buy-now">
+                            <button class="btn btn-primary" type="submit" name="buy-now-' . $productId . '-">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"/>
                                     <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1h-.003zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195l.054.012z"/>
